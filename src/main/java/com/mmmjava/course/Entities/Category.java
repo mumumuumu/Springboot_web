796fbe9c085da00f353a75a093e7ff4f344bd69d
Murilo Mognon <mognonmmurilo@gmail.com>
2025-03-19 16:51:58 -0300

    Associação manytomany category product

--- src/main/java/com/mmmjava/course/Entities/Category.java
+++ src/main/java/com/mmmjava/course/Entities/Category.java
@@ -1,5 +1,6 @@
 package com.mmmjava.course.Entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,8 +17,8 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category(){
