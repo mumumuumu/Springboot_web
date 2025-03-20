dfe60bf0743fb38662b6653d4dbb2dc7174f7e07
Murilo Mognon <mognonmmurilo@gmail.com>
2025-03-20 15:03:08 -0300

    Subtotal e total

--- src/main/java/com/mmmjava/course/Entities/Order.java
+++ src/main/java/com/mmmjava/course/Entities/Order.java
@@ -85,6 +85,14 @@ public class Order implements Serializable {
     public void setPayment(Payment payment) {
         this.payment = payment;
     }
+    public Double getTotal(){
+        double sum = 0.0;
+        for(OrderItem x : items){
+            sum += x.getSubTotal();
+        }
+        return sum;
+
+    }
 
     @Override
     public boolean equals(Object o) {
--- src/main/java/com/mmmjava/course/Entities/OrderItem.java
+++ src/main/java/com/mmmjava/course/Entities/OrderItem.java
@@ -55,6 +55,10 @@ public class OrderItem implements Serializable {
         this.quantity = quantity;
     }
 
+    public Double getSubTotal(){
+        return price * quantity;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
